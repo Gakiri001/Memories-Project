eb1cd03d7f179fcd57b74d4dea694003129204ed
Gakiri001 <gakirijulius@gmail.com>
2024-10-05 19:07:02 +0300

    client setup and mongoDB connection

--- server/index.js
+++ server/index.js
@@ -3,4 +3,12 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+const app = express();
 
+app.use(bodyParser.json({limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(cors());
+
+//https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = 'mongodb+srv://Julius:Julius123@cluster0.qsjub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
