55d13fe2f241a1e8a81af845fd46f36865a37ba1
Gakiri001 <gakirijulius@gmail.com>
2024-10-11 18:44:52 +0300

    seeting up the server port

--- server/index.js
+++ server/index.js
@@ -12,3 +12,8 @@ app.use(cors());
 //https://www.mongodb.com/cloud/atlas
 
 const CONNECTION_URL = 'mongodb+srv://Julius:Julius123@cluster0.qsjub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {})
+  .then(() => app.listen(PORT, () => console.log(`server running on ${PORT}`)))
+  .catch((error) => console.log(error.message))
